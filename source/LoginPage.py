0bc30b1c972ed7b2a659b03384efd7d56ff71e2a
sanket.de <sanket.de@outlook.com>
2018-02-22 00:26:26 +0530

    added method for LoginButton with response to dropdown select option.

--- source/LoginPage.py
+++ source/LoginPage.py
@@ -1,17 +1,41 @@
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets,QtSql
 from ui.UiLoginPage import Ui_MainWindow
 
 
 class Main(QtWidgets.QMainWindow):
+
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         self.ui =Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.pushButton_Login.clicked.connect(self.onClick)  #Connecting the button to action.
+        self.ui.comboBox_UserType.addItem("Admin_Login")
+        self.ui.comboBox_UserType.addItem("Officer_Login")
+        self.ui.pushButton_Login.clicked.connect(self.onClickLoginIn)  #Connecting the button to action.
 
-    def onClick(self):  #The function is called when the button clicked
-        self.ui.lineEdit_UserName.setText("Sanket_Darwante")
+    def onClickLoginIn(self):  #The function is called when the button clicked
+        username=self.ui.lineEdit_UserName.text()
+        password=self.ui.lineEdit_UserName.text()
+        Admin_Login = "Admin_Login"
+        Officer_Login = "Officer_Login"
+        text=self.ui.comboBox_UserType.currentText()
+        if (text==Admin_Login):
+            self.authAdmin()
+        elif (text==Officer_Login):
+            self.authOfficer()
+
+    def authAdmin(self):
+        pass
+
+    def authOfficer(self):
+        pass
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
